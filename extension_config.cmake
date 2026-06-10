d7d3b6f3fafea31b8844194c5fe20da650ec89d3
ben fleis <benfleis@users.noreply.github.com>
2026-06-10 14:33:21 +0200

    fix duckdb handling of ratified commits in managed tables
    In order to handle visible ratified commits within a UC managed table,
    delta-kernel requires the `max_catalog_version` parameter. Get this,
    and pass it through to delta+kernel. Depends on duckdb/duckdb-delta#319
    
    New test validates against both spark- and duckdb- committed tables.

--- extension_config.cmake
+++ extension_config.cmake
@@ -8,7 +8,5 @@ duckdb_extension_load(unity_catalog
 
 # NOTE: replace with SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../delta for local dev
 duckdb_extension_load(delta
-    GIT_URL https://github.com/duckdb/duckdb-delta
-    GIT_TAG 4cfde24f98b5128c46a7a06a67e292024f920b1e
-    SUBMODULES extension-ci-tools
+    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../delta
 )
