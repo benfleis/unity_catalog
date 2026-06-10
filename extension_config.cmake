e0feecb611406b0c10c2505b1be853a461e52bfc
ben fleis <benfleis@users.noreply.github.com>
2026-06-10 17:16:26 +0200

    update delta git ref

--- extension_config.cmake
+++ extension_config.cmake
@@ -8,7 +8,7 @@ duckdb_extension_load(unity_catalog
 
 # NOTE: replace with SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../delta for local dev
 duckdb_extension_load(delta
-    GIT_URL https://github.com/benfleis/duckdb-delta
-    GIT_TAG 0a77c6ec43a7febff36580960e5c1a8739d85420
+    GIT_URL https://github.com/duckdb/duckdb-delta
+    GIT_TAG 45c40878601b54b4188b09e08732fe0d576ad222
     SUBMODULES extension-ci-tools
 )
