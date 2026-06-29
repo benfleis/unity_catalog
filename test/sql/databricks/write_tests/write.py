1e375fdbd87606dc093ee270263851b0837c19af
ben fleis <benfleis@users.noreply.github.com>
2026-06-29 23:01:55 +0200

    fix plain write test

---
+++ test/sql/databricks/write_tests/write.py
@@ -0,0 +1,11 @@
+from driver import requires, run_paired
+
+
+@requires(
+    source="${UC_TEST_CATALOG}.source.simple_table",
+    access="rw",
+    commit="plain",
+    storage="external",
+)
+def test_write_catalog_managed(request, resources):
+    run_paired(request, env=resources.env)
