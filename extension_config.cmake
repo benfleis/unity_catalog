e3d3bea8601ee99be973cc9366787a05576456ab
ben fleis <benfleis@users.noreply.github.com>
2026-07-05 11:40:18 +0200

    include delta fwd fill changes

--- extension_config.cmake
+++ extension_config.cmake
@@ -11,6 +11,6 @@ duckdb_extension_load(delta
     #GIT_URL https://github.com/duckdb/duckdb-delta
     #GIT_TAG 45c40878601b54b4188b09e08732fe0d576ad222
     GIT_URL https://github.com/benfleis/duckdb-delta
-    GIT_TAG f9c52ae07992b11fc3f095f6a6520eafa7186fdd
+    GIT_TAG dee7b11dfa04e0ab467aa8c5dc04ce9083368ca5
     SUBMODULES extension-ci-tools
 )
